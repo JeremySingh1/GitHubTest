60e7017f351f141784007ed8dcef050010a7b570
Jeremy Singh <barstard@me.com>
2020-05-18 16:20:57 +1000

    First Commit

---
+++ GitHubTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubTest
+//
+//  Created by Jeremy Singh on 18/5/20.
+//  Copyright © 2020 Picture Factory. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
