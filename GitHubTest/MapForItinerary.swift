617f929162805aaad6bb9c60362b07e8fd367924
Jeremy Singh <barstard@me.com>
2020-05-18 16:29:43 +1000

    Added Map Feature

---
+++ GitHubTest/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitHubTest
+//
+//  Created by Jeremy Singh on 18/5/20.
+//  Copyright © 2020 Picture Factory. All rights reserved.
+//
+
+import MapKit
+
+class MapForItinerary: NSObject {
+    
+}
